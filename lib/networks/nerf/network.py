b3486fa055eab7289ed2c1151cc8c8a75ef927fd
xsqhust <129826877+xsqhust@users.noreply.github.com>
2024-04-10 18:44:07 +0800

    Add files via upload

---
+++ lib/networks/nerf/network.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import numpy as np
+from lib.networks.encoding import get_encoder
+from lib.config import cfg
+
+
+
+
+
+class NeRF(nn.Module):
+    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, skips=[4], use_viewdirs=False):
+        super(NeRF, self).__init__()
+        self.D = D
+        self.W = W
+        self.input_ch = input_ch
+        self.input_ch_views = input_ch_views
+        self.skips = skips
+        self.use_viewdirs = use_viewdirs
+        self.output_ch = 5 if self.use_viewdirs else 4
+
+        self.pts_linears = nn.ModuleList(
+        [nn.Linear(self.input_ch, self.W)] + [nn.Linear(self.W, self.W) if i not in self.skips else nn.Linear(self.W + self.input_ch, self.W) for i in
+                                        range(self.D - 1)])
+
+        self.views_linears = nn.ModuleList([nn.Linear(self.input_ch_views + self.W, self.W // 2)])
+
+        if self.use_viewdirs:
+            # feature vector(256)
+            self.feature_linear = nn.Linear(self.W, self.W)
+            # alpha(1)
+            self.alpha_linear = nn.Linear(self.W, 1)
+            # rgb color(3)
+            self.rgb_linear = nn.Linear(self.W // 2, 3)
+        else:
+            # output channel(default: 4)
+            self.output_linear = nn.Linear(self.W, self.output_ch)
+
+
+
+    def forward(self, x):
+        input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
+        h = input_pts
+        for i, l in enumerate(self.pts_linears):
+            h = self.pts_linears[i](h)
+            h = F.relu(h)
+            if i in self.skips:
+                h = torch.cat([input_pts, h], -1)
+
+        if self.use_viewdirs:
+            alpha = self.alpha_linear(h)
+            feature = self.feature_linear(h)
+            h = torch.cat([feature, input_views], -1)
+
+            for i, l in enumerate(self.views_linears):
+                h = self.views_linears[i](h)
+                h = F.relu(h)
+
+            rgb = self.rgb_linear(h)
+            outputs = torch.cat([rgb, alpha], -1)
+        else:
+            outputs = self.output_linear(h)
+
+        return outputs
+
+
+
+
+
+
+
