54b362c5de32162bdb40464ac1a32a40ee49eafc
xsqhust <129826877+xsqhust@users.noreply.github.com>
2024-04-12 10:21:19 +0800

    Add files via upload

--- lib/networks/nerf/network.py
+++ lib/networks/nerf/network.py
@@ -5,9 +5,9 @@ import numpy as np
 from lib.networks.encoding import get_encoder
 from lib.config import cfg
 
+from .run_nerf_helpers import *
 
-
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class NeRF(nn.Module):
     def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, skips=[4], use_viewdirs=False):
@@ -19,19 +19,20 @@ class NeRF(nn.Module):
         self.skips = skips
         self.use_viewdirs = use_viewdirs
         self.output_ch = 5 if self.use_viewdirs else 4
-
+        # 定义位置编码后的位置信息的线性层，如果层数在skips列表中，则将原始位置信息与隐藏层拼接
         self.pts_linears = nn.ModuleList(
         [nn.Linear(self.input_ch, self.W)] + [nn.Linear(self.W, self.W) if i not in self.skips else nn.Linear(self.W + self.input_ch, self.W) for i in
                                         range(self.D - 1)])
-
+        # 定义位置编码后的视角方向信息的线性层
         self.views_linears = nn.ModuleList([nn.Linear(self.input_ch_views + self.W, self.W // 2)])
 
         if self.use_viewdirs:
             # feature vector(256)
+             # 定义特征向量的线性层
             self.feature_linear = nn.Linear(self.W, self.W)
-            # alpha(1)
+             # 定义透明度（alpha）值的线性层
             self.alpha_linear = nn.Linear(self.W, 1)
-            # rgb color(3)
+            # 定义RGB颜色的线性层
             self.rgb_linear = nn.Linear(self.W // 2, 3)
         else:
             # output channel(default: 4)
@@ -40,29 +41,284 @@ class NeRF(nn.Module):
 
 
     def forward(self, x):
+        # 提取位置和视角方向信息
         input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
+
+        # ======位置信息输入网络========
         h = input_pts
         for i, l in enumerate(self.pts_linears):
             h = self.pts_linears[i](h)
             h = F.relu(h)
             if i in self.skips:
-                h = torch.cat([input_pts, h], -1)
-
+                h = torch.cat([input_pts, h], -1)  # 如果层数在skips列表中，则将原始位置信息与隐藏层拼接
+        # ======使用方向信息========
         if self.use_viewdirs:
-            alpha = self.alpha_linear(h)
-            feature = self.feature_linear(h)
-            h = torch.cat([feature, input_views], -1)
-
+            alpha = self.alpha_linear(h)  # 预测透明度
+            feature = self.feature_linear(h)  # 预测特征向量
+            h = torch.cat([feature, input_views], -1)# 将特征向量与视觉方向拼接
+             # ======方向信息输入网络========
             for i, l in enumerate(self.views_linears):
                 h = self.views_linears[i](h)
                 h = F.relu(h)
 
+        #=========预测rgb并将rgb和透明度进行拼接
             rgb = self.rgb_linear(h)
             outputs = torch.cat([rgb, alpha], -1)
         else:
             outputs = self.output_linear(h)
 
         return outputs
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.N_samples = cfg.task_arg.N_samples # 粗采样点
+        self.N_importance = cfg.task_arg.N_importance # 精细采样点
+        self.chunk = cfg.task_arg.chunk_size # 并行处理4096个射线  
+        self.batch_size = cfg.task_arg.N_rays # batchsize训练大小为  1024个射线  
+        self.white_bkgd = cfg.task_arg.white_bkgd # 用白色背景
+        self.use_viewdirs = cfg.task_arg.use_viewdirs # 是否加入相机方向信息
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # =========================对输入相机位置以及方向进行编码=========================
+        self.embed_fn, self.input_ch = get_encoder(cfg.network.xyz_encoder)
+        self.embeddirs_fn, self.input_ch_views = get_encoder(cfg.network.dir_encoder)
+
+        # coarse model
+        self.model = NeRF(D=cfg.network.nerf.D_c,
+                          W=cfg.network.nerf.W_c,
+                          input_ch=self.input_ch,
+                          input_ch_views=self.input_ch_views,
+                          skips=cfg.network.nerf.skips,
+                          use_viewdirs=self.use_viewdirs)
+
+        # fine model
+        self.model_fine = NeRF(D=cfg.network.nerf.D_f,
+                               W=cfg.network.nerf.W_f,
+                               input_ch=self.input_ch,
+                               input_ch_views=self.input_ch_views,
+                               skips=cfg.network.nerf.skips,
+                               use_viewdirs=self.use_viewdirs)
+
+
+    def batchify(self, fn, chunk):
+        """Constructs a version of 'fn' that applies to smaller batches."""
+        def ret(inputs):
+            return torch.cat([fn(inputs[i:i + chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
+        return ret
+
+
+    def forward_network(self, inputs, viewdirs, model=''):
+        """Prepares inputs and applies network 'fn'."""
+        if model == 'fine':
+            fn = self.model_fine
+        else:
+            fn = self.model
+
+        inputs_flat = torch.reshape(inputs, [-1, inputs.shape[-1]])
+        embedded = self.embed_fn(inputs_flat)
+
+        if self.use_viewdirs:
+            input_dirs = viewdirs[:, None].expand(inputs.shape)
+            input_dirs_flat = torch.reshape(input_dirs, [-1, input_dirs.shape[-1]])
+            embedded_dirs = self.embeddirs_fn(input_dirs_flat)
+            embedded = torch.cat([embedded, embedded_dirs], -1)
+
+        embedded = embedded.to(torch.float32)
+        outputs_flat = self.batchify(fn, self.chunk)(embedded)
+        outputs = torch.reshape(outputs_flat, list(inputs.shape[:-1]) + [outputs_flat.shape[-1]])
+        return outputs
+
+
+    def render_rays(self,ray_batch,
+        N_samples= cfg.task_arg.N_samples,  #每条光线的采样数
+        retraw=False,
+        lindisp=cfg.task_arg.lindisp,
+        perturb=cfg.task_arg.perturb,
+        N_importance=cfg.task_arg.N_importance,
+        white_bkgd= cfg.task_arg.white_bkgd,
+        raw_noise_std= cfg.task_arg.raw_noise_std,
+        pytest=False):
+        """Volumetric rendering.
+        Args:
+        ray_batch: array of shape [batch_size, ...]. All information necessary
+            for sampling along a ray, including: ray origin, ray direction, min
+            dist, max dist, and unit-magnitude viewing direction.
+        network_fn: function. Model for predicting RGB and density at each point
+            in space.
+        network_query_fn: function used for passing queries to network_fn.
+        N_samples: int. Number of different times to sample along each ray.
+        retraw: bool. If True, include model's raw, unprocessed predictions.
+        lindisp: bool. If True, sample linearly in inverse depth rather than in depth.
+        perturb: float, 0 or 1. If non-zero, each ray is sampled at stratified
+            random points in time.
+        N_importance: int. Number of additional times to sample along each ray.
+            These samples are only passed to network_fine.
+        network_fine: "fine" network with same spec as network_fn.
+        white_bkgd: bool. If True, assume a white background.
+        raw_noise_std: ...
+        verbose: bool. If True, print more debugging info.
+        Returns:
+        rgb_map: [num_rays, 3]. Estimated RGB color of a ray. Comes from fine model.
+        disp_map: [num_rays]. Disparity map. 1 / depth.
+        acc_map: [num_rays]. Accumulated opacity along each ray. Comes from fine model.
+        raw: [num_rays, num_samples, 4]. Raw predictions from model.
+        rgb0: See rgb_map. Output for coarse model.
+        disp0: See disp_map. Output for coarse model.
+        acc0: See acc_map. Output for coarse model.
+        z_std: [num_rays]. Standard deviation of distances along ray for each
+            sample.
+        """
+        
+        N_rays = ray_batch.shape[0]   #射线得数量
+        rays_o, rays_d = ray_batch[:,0:3], ray_batch[:,3:6] # [N_rays, 3] 射线得起点以及方向
+        viewdirs = ray_batch[:,-3:] if ray_batch.shape[-1] > 8 else None   # 方向信息
+        bounds = torch.reshape(ray_batch[...,6:8], [-1,1,2])   
+        near, far = bounds[...,0], bounds[...,1] # [-1,1]
+
+        t_vals = torch.linspace(0., 1., steps=N_samples)
+        t_vals = t_vals.to(device)
+        if not lindisp:
+            z_vals = near * (1.-t_vals) + far * (t_vals)
+        else:
+            z_vals = 1./(1./near * (1.-t_vals) + 1./far * (t_vals))
+
+        z_vals = z_vals.expand([N_rays, N_samples])
+
+        if perturb > 0.:
+            # get intervals between samples
+            mids = .5 * (z_vals[...,1:] + z_vals[...,:-1])
+            upper = torch.cat([mids, z_vals[...,-1:]], -1)
+            lower = torch.cat([z_vals[...,:1], mids], -1)
+            # stratified samples in those intervals
+            t_rand = torch.rand(z_vals.shape)
+            t_rand = t_rand.to(device)
+            # Pytest, overwrite u with numpy's fixed random numbers
+            if pytest:
+                np.random.seed(0)
+                t_rand = np.random.rand(*list(z_vals.shape))
+                t_rand = torch.Tensor(t_rand)
+
+            z_vals = lower + (upper - lower) * t_rand
+
+        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples, 3]
+
+
+    #     raw = run_network(pts)
+        raw = self.forward_network(pts, viewdirs)
+        rgb_map, disp_map, acc_map, weights, depth_map = raw2outputs(raw, z_vals, rays_d, raw_noise_std, white_bkgd, pytest=pytest)
+
+        if N_importance > 0:
+
+            rgb_map_0, disp_map_0, acc_map_0 = rgb_map, disp_map, acc_map
+
+            z_vals_mid = .5 * (z_vals[...,1:] + z_vals[...,:-1])
+            z_samples = sample_pdf(z_vals_mid, weights[...,1:-1], N_importance, det=(perturb==0.), pytest=pytest)
+            z_samples = z_samples.detach()
+
+            z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
+            pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples + N_importance, 3]
+
+           
+    #         raw = run_network(pts, fn=run_fn)
+            raw = self.forward_network(pts, viewdirs, model='fine')
+
+            rgb_map, disp_map, acc_map, weights, depth_map = raw2outputs(raw, z_vals, rays_d, raw_noise_std, white_bkgd, pytest=pytest)
+
+        ret = {'rgb_map' : rgb_map, 'disp_map' : disp_map, 'acc_map' : acc_map}
+        if retraw:
+            ret['raw'] = raw
+        if N_importance > 0:
+            ret['rgb0'] = rgb_map_0
+            ret['disp0'] = disp_map_0
+            ret['acc0'] = acc_map_0
+            ret['z_std'] = torch.std(z_samples, dim=-1, unbiased=False)  # [N_rays]
+
+        for k in ret:
+            DEBUG= False
+            if (torch.isnan(ret[k]).any() or torch.isinf(ret[k]).any()) and DEBUG:
+                print(f"! [Numerical Error] {k} contains nan or inf.")
+
+        return ret
+
+
+
+    def batchify_rays(self,rays_flat, chunk=1024*32):
+        """Render rays in smaller minibatches to avoid OOM.
+        """
+        all_ret = {}
+        for i in range(0, rays_flat.shape[0], chunk):
+            ret = self.render_rays(rays_flat[i:i+chunk])
+            for k in ret:
+                if k not in all_ret:
+                    all_ret[k] = []
+                all_ret[k].append(ret[k])
+
+        all_ret = {k : torch.cat(all_ret[k], 0) for k in all_ret}
+        return all_ret
+
+
+    def render(self,batch):
+        """Render rays
+        Args:
+        H: int. Height of image in pixels.
+        W: int. Width of image in pixels.
+        focal: float. Focal length of pinhole camera.
+        chunk: int. Maximum number of rays to process simultaneously. Used to
+            control maximum memory usage. Does not affect final results.
+        rays: array of shape [2, batch_size, 3]. Ray origin and direction for
+            each example in batch.
+        c2w: array of shape [3, 4]. Camera-to-world transformation matrix.
+        ndc: bool. If True, represent ray origin, direction in NDC coordinates.
+        near: float or array of shape [batch_size]. Nearest distance for a ray.
+        far: float or array of shape [batch_size]. Farthest distance for a ray.
+        use_viewdirs: bool. If True, use viewing direction of a point in space in model.
+        c2w_staticcam: array of shape [3, 4]. If not None, use this transformation matrix for 
+        camera while using other c2w argument for viewing directions.
+        Returns:
+        rgb_map: [batch_size, 3]. Predicted RGB values for rays.
+        disp_map: [batch_size]. Disparity map. Inverse of depth.
+        acc_map: [batch_size]. Accumulated opacity (alpha) along a ray.
+        extras: dict with everything returned by render_rays().
+        """
+        rays_o = batch['ray_o']
+        rays_d = batch['ray_d']
+        near = batch['near']
+        far = batch['far']
+
+        viewdirs = rays_d 
+        viewdirs = viewdirs / torch.norm(viewdirs, dim=-1, keepdim=True)
+        viewdirs = torch.reshape(viewdirs, [-1,3]).float()
+
+        sh = rays_d.shape # [..., 3]
+        # Create ray batch
+        rays_o = torch.reshape(rays_o, [-1,3]).float()
+        rays_d = torch.reshape(rays_d, [-1,3]).float()
+
+        near, far = near * torch.ones_like(rays_d[...,:1]), far * torch.ones_like(rays_d[...,:1])
+        rays = torch.cat([rays_o, rays_d, near, far], -1)
+        if self.use_viewdirs:
+            rays = torch.cat([rays, viewdirs], -1)
+
+        # Render and reshape
+        all_ret = self.batchify_rays(rays,self.chunk)
+        for k in all_ret:
+            k_sh = list(sh[:-1]) + list(all_ret[k].shape[1:])
+            all_ret[k] = torch.reshape(all_ret[k], k_sh)
+
+        # k_extract = ['rgb_map', 'disp_map', 'acc_map']
+        # ret_list = [all_ret[k] for k in k_extract]
+        # ret_dict = {k : all_ret[k] for k in all_ret if k not in k_extract}
+        return all_ret
+    #===============================体渲染======================
+
+
+
+
+
+
+
 
 
 
---
+++ lib/networks/nerf/run_nerf_helpers.py
@@ -0,0 +1,205 @@
+import torch
+# torch.autograd.set_detect_anomaly(True)
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Misc
+img2mse = lambda x, y : torch.mean((x - y) ** 2)
+mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
+
+
+# Positional encoding (section 5.1)
+class Embedder:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.create_embedding_fn()
+        
+    def create_embedding_fn(self):
+        embed_fns = []
+        d = self.kwargs['input_dims']
+        out_dim = 0
+        if self.kwargs['include_input']:
+            embed_fns.append(lambda x : x)
+            out_dim += d
+            
+        max_freq = self.kwargs['max_freq_log2']
+        N_freqs = self.kwargs['num_freqs']
+        
+        if self.kwargs['log_sampling']:
+            freq_bands = 2.**torch.linspace(0., max_freq, steps=N_freqs)
+        else:
+            freq_bands = torch.linspace(2.**0., 2.**max_freq, steps=N_freqs)
+            
+        for freq in freq_bands:
+            for p_fn in self.kwargs['periodic_fns']:
+                embed_fns.append(lambda x, p_fn=p_fn, freq=freq : p_fn(x * freq))
+                out_dim += d
+                    
+        self.embed_fns = embed_fns
+        self.out_dim = out_dim
+        
+    def embed(self, inputs):
+        return torch.cat([fn(inputs) for fn in self.embed_fns], -1)
+
+
+def get_embedder(multires, i=0):
+    if i == -1:
+        return nn.Identity(), 3
+    
+    embed_kwargs = {
+                'include_input' : True,
+                'input_dims' : 3,
+                'max_freq_log2' : multires-1,
+                'num_freqs' : multires,
+                'log_sampling' : True,
+                'periodic_fns' : [torch.sin, torch.cos],
+    }
+    
+    embedder_obj = Embedder(**embed_kwargs)
+    embed = lambda x, eo=embedder_obj : eo.embed(x)
+    return embed, embedder_obj.out_dim
+
+
+
+# Ray helpers
+def get_rays(H, W, K, c2w):
+    i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))  # pytorch's meshgrid has indexing='ij'
+    i = i.t()
+    j = j.t()
+    dirs = torch.stack([(i-K[0][2])/K[0][0], -(j-K[1][2])/K[1][1], -torch.ones_like(i)], -1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
+    rays_o = c2w[:3,-1].expand(rays_d.shape)
+    return rays_o, rays_d
+
+
+def get_rays_np(H, W, K, c2w):
+    i, j = np.meshgrid(np.arange(W, dtype=np.float32), np.arange(H, dtype=np.float32), indexing='xy')
+    dirs = np.stack([(i-K[0][2])/K[0][0], -(j-K[1][2])/K[1][1], -np.ones_like(i)], -1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = np.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
+    rays_o = np.broadcast_to(c2w[:3,-1], np.shape(rays_d))
+    return rays_o, rays_d
+
+
+def ndc_rays(H, W, focal, near, rays_o, rays_d):
+    # Shift ray origins to near plane
+    t = -(near + rays_o[...,2]) / rays_d[...,2]
+    rays_o = rays_o + t[...,None] * rays_d
+    
+    # Projection
+    o0 = -1./(W/(2.*focal)) * rays_o[...,0] / rays_o[...,2]
+    o1 = -1./(H/(2.*focal)) * rays_o[...,1] / rays_o[...,2]
+    o2 = 1. + 2. * near / rays_o[...,2]
+
+    d0 = -1./(W/(2.*focal)) * (rays_d[...,0]/rays_d[...,2] - rays_o[...,0]/rays_o[...,2])
+    d1 = -1./(H/(2.*focal)) * (rays_d[...,1]/rays_d[...,2] - rays_o[...,1]/rays_o[...,2])
+    d2 = -2. * near / rays_o[...,2]
+    
+    rays_o = torch.stack([o0,o1,o2], -1)
+    rays_d = torch.stack([d0,d1,d2], -1)
+    
+    return rays_o, rays_d
+
+
+# Hierarchical sampling (section 5.2)
+def sample_pdf(bins, weights, N_samples, det=False, pytest=False):
+    # Get pdf
+    weights = weights + 1e-5 # prevent nans
+    pdf = weights / torch.sum(weights, -1, keepdim=True)
+    cdf = torch.cumsum(pdf, -1)
+    cdf = torch.cat([torch.zeros_like(cdf[...,:1]), cdf], -1)  # (batch, len(bins))
+
+    # Take uniform samples
+    if det:
+        u = torch.linspace(0., 1., steps=N_samples)
+        u = u.expand(list(cdf.shape[:-1]) + [N_samples])
+    else:
+        u = torch.rand(list(cdf.shape[:-1]) + [N_samples])
+
+    # Pytest, overwrite u with numpy's fixed random numbers
+    if pytest:
+        np.random.seed(0)
+        new_shape = list(cdf.shape[:-1]) + [N_samples]
+        if det:
+            u = np.linspace(0., 1., N_samples)
+            u = np.broadcast_to(u, new_shape)
+        else:
+            u = np.random.rand(*new_shape)
+        u = torch.Tensor(u)
+
+    # Invert CDF
+    u = u.contiguous()
+    u=u.to(device)
+    inds = torch.searchsorted(cdf, u, right=True)
+    below = torch.max(torch.zeros_like(inds-1), inds-1)
+    above = torch.min((cdf.shape[-1]-1) * torch.ones_like(inds), inds)
+    inds_g = torch.stack([below, above], -1)  # (batch, N_samples, 2)
+
+    # cdf_g = tf.gather(cdf, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    # bins_g = tf.gather(bins, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    matched_shape = [inds_g.shape[0], inds_g.shape[1], cdf.shape[-1]]
+    cdf_g = torch.gather(cdf.unsqueeze(1).expand(matched_shape), 2, inds_g)
+    bins_g = torch.gather(bins.unsqueeze(1).expand(matched_shape), 2, inds_g)
+
+    denom = (cdf_g[...,1]-cdf_g[...,0])
+    denom = torch.where(denom<1e-5, torch.ones_like(denom), denom)
+    t = (u-cdf_g[...,0])/denom
+    samples = bins_g[...,0] + t * (bins_g[...,1]-bins_g[...,0])
+
+    return samples
+
+
+
+
+
+def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=False):
+    """Transforms model's predictions to semantically meaningful values.
+    Args:
+        raw: [num_rays, num_samples along ray, 4]. Prediction from model.
+        z_vals: [num_rays, num_samples along ray]. Integration time.
+        rays_d: [num_rays, 3]. Direction of each ray.
+    Returns:
+        rgb_map: [num_rays, 3]. Estimated RGB color of a ray.
+        disp_map: [num_rays]. Disparity map. Inverse of depth map.
+        acc_map: [num_rays]. Sum of weights along each ray.
+        weights: [num_rays, num_samples]. Weights assigned to each sampled color.
+        depth_map: [num_rays]. Estimated distance to object.
+    """
+    raw2alpha = lambda raw, dists, act_fn=F.relu: 1.-torch.exp(-act_fn(raw)*dists)
+
+    dists = z_vals[...,1:] - z_vals[...,:-1]
+    dists = torch.cat([dists, torch.Tensor([1e10]).to(device).expand(dists[...,:1].shape)], -1)  # [N_rays, N_samples]
+
+    dists = dists * torch.norm(rays_d[...,None,:], dim=-1)
+
+    rgb = torch.sigmoid(raw[...,:3])  # [N_rays, N_samples, 3]
+    noise = 0.
+    if raw_noise_std > 0.:
+        noise = torch.randn(raw[...,3].shape) * raw_noise_std
+
+        # Overwrite randomly sampled data if pytest
+        if pytest:
+            np.random.seed(0)
+            noise = np.random.rand(*list(raw[...,3].shape)) * raw_noise_std
+            noise = torch.Tensor(noise)
+
+    alpha = raw2alpha(raw[...,3] + noise, dists)  # [N_rays, N_samples]
+    # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
+    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)).to(device), 1.-alpha + 1e-10], -1), -1)[:, :-1]
+    rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
+
+    depth_map = torch.sum(weights * z_vals, -1)
+    disp_map = 1./torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
+    acc_map = torch.sum(weights, -1)
+
+    if white_bkgd:
+        rgb_map = rgb_map + (1.-acc_map[...,None])
+
+    return rgb_map, disp_map, acc_map, weights, depth_map
+
